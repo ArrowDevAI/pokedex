67eecebf06be97bcd5abd8279351697f68b3279f
Trevor Higgins <higginst.th@gmail.com>
2024-04-25 07:25:47 -0700

    updated scripts
    added list-group item class to listpokemon, added btn classes, created showModal funcition

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
-
+``
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
@@ -18,18 +18,21 @@ let pokemonRepository = (function () {
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
+    listpokemon.classList.add("list-group-item")
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add("button-class");
+    button.classList.add("btn","btn-dark","button-class");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
     button.addEventListener("click", function (event) {
-      showDetails(pokemon);
+    showDetails(pokemon);
+  ;
     });
   }
 
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
+    return fetch(apiUrl)
+    .then(function (response) {
       return response.json();
     }).then(function (json) {
       json.results.forEach(function (item) {
@@ -61,9 +64,21 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    pokemonRepository.loadDetails(pokemon).then(function () {
-      modalRepo.showModal(pokemon.name, "Height:" + pokemon.height, pokemon.imageUrl);
+    loadDetails(pokemon).then(function () {
+      showModal(pokemon);
     });
+  }
+
+  function showModal(pokemon) {
+    pokemonModalBody.innerHTML = `
+      <img src="${pokemon.imageUrl}" alt="${pokemon.name}" style="max-width: 100%;">
+      <p>Height: ${pokemon.height}</p>
+    `;
+    modalHeader.innerHTML = `
+    <p>${pokemon.name}</p>
+    `;
+    let pokemonModal = new bootstrap.Modal(document.getElementById('pokemonModal'));
+    pokemonModal.show();
   }
 
   return {
@@ -76,70 +91,9 @@ let pokemonRepository = (function () {
   };
 })();
 
-
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
 
-
-let modalRepo = (function () {
-  function showModal(title, text, imageUrl) {
-    let modalContainer = document.querySelector('#modal-container');
-
-    modalContainer.innerHTML = '';
-
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    // Add the new modal content
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    let imageElement = document.createElement('img');
-    imageElement.src = imageUrl;
-    imageElement.alt = title + " image"
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modal.appendChild(imageElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-
-    modalContainer.addEventListener('click', (e) => {
-      let target = e.target;
-      if (target === modalContainer) {
-        hideModal();
-      }
-    });
-
-  }
-
-
-  function hideModal() {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    let modalContainer = document.querySelector('#modal-container');
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-  return {
-    showModal,
-    hideModal,
-  };
-})();
